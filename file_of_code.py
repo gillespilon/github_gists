b11722b0275d2933b5986ec3aa3779ad5c2ed8ae
Gilles Pilon <gillespilon13@gmail.com>
2021-05-10 11:51:53 -0500

    Add five conditions to evaluate

--- file_of_code.py
+++ file_of_code.py
@@ -6,6 +6,12 @@ Example of a script with functions.
 
 def main():
     print(capital_case('gilles'))
+    print(capital_case('GILLES'))
+    print(capital_case('gIlLeS'))
+    print(capital_case('good morning'))
+    print(capital_case('good Morning'))
+    print(capital_case('Good Morning'))
+    print(capital_case('Good morning'))
 
 
 def capital_case(x: str) -> str:
