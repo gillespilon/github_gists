60c805e5af46c2c73f79530d66d758f3e00a2abe
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-08-25 13:15:10 -0500

    Tidy code

--- tests/def_test_capitalize.py
+++ tests/def_test_capitalize.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
-'''
-Test functions for file_of_code.py
-'''
+"""
+Test for def capitalize()
+"""
 
-import file_of_code as fc
+import capitalize_string as cs
 
 
 def test_capital_case():
@@ -11,11 +11,11 @@ def test_capital_case():
     The capitalize() method converts the first character of the string to
     uppercase while making all other characters lowercase.
     '''
-    assert fc.capital_case('gilles') == 'Gilles'
-    assert fc.capital_case('GILLES') == 'Gilles'
-    assert fc.capital_case('gIlLeS') == 'Gilles'
-    assert fc.capital_case('Gilles') == 'Gilles'
-    assert fc.capital_case('good morning') == 'Good morning'
-    assert fc.capital_case('good Morning') == 'Good morning'
-    assert fc.capital_case('Good Morning') == 'Good morning'
-    assert fc.capital_case('Good morning') == 'Good morning'
+    assert cs.capital_case('gilles') == 'Gilles'
+    assert cs.capital_case('GILLES') == 'Gilles'
+    assert cs.capital_case('gIlLeS') == 'Gilles'
+    assert cs.capital_case('Gilles') == 'Gilles'
+    assert cs.capital_case('good morning') == 'Good morning'
+    assert cs.capital_case('good Morning') == 'Good morning'
+    assert cs.capital_case('Good Morning') == 'Good morning'
+    assert cs.capital_case('Good morning') == 'Good morning'
